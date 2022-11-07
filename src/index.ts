b5ca50f0f0e82d004f36e4cea1ab80046894d511
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2022-11-07 16:00:41 +0330

    reformat over

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 // import * as _ from 'lodash';
 import './style/style.scss'
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
 const observable = new Observable(subscriber => {
-    let counter = 0;
-    setTimeout(() => {
-        counter +=20;
-        subscriber.next(`counter value changed to = ${counter}`)
-    },2000)
-    setTimeout(() => {
-        counter +=50;
-        subscriber.next(`counter value changed to = ${counter}`)
-    },4000)
-    setTimeout(() => {
-        counter +=50;
-        subscriber.next(`counter value changed to = ${counter}`)
-    },8000)
-    setTimeout(() => {
-        subscriber.next('event is compelete')
-        subscriber.complete()
-    },9000)
-    //this is not work beacause event is compelete
-    setTimeout(() => {
-        counter +=50;
-        subscriber.error(`counter value changed to = ${counter}`)
-    },10000)
+  let counter = 0;
+  setTimeout(() => {
+    counter += 20;
+    subscriber.next(`counter value changed to = ${counter}`)
+  }, 2000)
+  setTimeout(() => {
+    counter += 50;
+    subscriber.next(`counter value changed to = ${counter}`)
+  }, 4000)
+  setTimeout(() => {
+    counter += 50;
+    subscriber.next(`counter value changed to = ${counter}`)
+  }, 8000)
+  setTimeout(() => {
+    subscriber.next('event is compelete')
+    subscriber.complete()
+  }, 9000)
+  //this is not work beacause event is compelete
+  setTimeout(() => {
+    counter += 50;
+    subscriber.error(`counter value changed to = ${counter}`)
+  }, 10000)
 })
 observable.subscribe(subscribe => {
-    console.log(subscribe)
+  console.log(subscribe)
 })

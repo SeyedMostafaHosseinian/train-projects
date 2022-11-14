faa16c1553a5eaaadd8b338d6edffe234b7cb07f
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2022-11-14 18:17:20 +0330

    add more example ...

--- session2-operators-all-train/src/multicastingOperators/shareOperator.ts
+++ session2-operators-all-train/src/multicastingOperators/shareOperator.ts
@@ -1,21 +1,21 @@
-import { from, interval, timer } from 'rxjs';
+import { from, interval, map, of, timer } from 'rxjs';
 import { tap, mapTo, share } from 'rxjs/operators';
 
 export function shareOperatorFun() {
 
-
-  const source = timer(1000);
-
+  const source = interval(1000);
+  //unshared observable
   const example = source.pipe(
-    tap(() => console.log('***SIDE EFFECT***')),
-    mapTo('***RESULT***')
+    tap(() => console.log('*** an new observable is created ***')),
+
   );
 
-  const subscribe = example.subscribe(val => console.log(val));
-  const subscribeTwo = example.subscribe(val => console.log(val));
+  const subscribe = example.subscribe(val => console.log('subscriber 1',`value : ${val}`));
+  const subscribeTwo = example.subscribe(val => console.log('subscriber 2',` value : ${val}`));
 
+  //sharing the observable
   const sharedExample = example.pipe(share());
 
-  const subscribeThree = sharedExample.subscribe(val => console.log(val));
-  const subscribeFour = sharedExample.subscribe(val => console.log(val));
+  const subscribeThree = sharedExample.subscribe(val => console.log('subscriber 3',` value : ${val}`));
+  const subscribeFour = sharedExample.subscribe(val => console.log('subscriber 4',` value : ${val}`));
 }

32987404322955d110c8b2dd053a79f4ab8d8de2
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2022-11-16 18:04:13 +0330

    add more ...

--- session2-subject-train/src/main.ts
+++ session2-subject-train/src/main.ts
@@ -4,8 +4,25 @@ import {
   replaySubjectTrain,
   asyncSubjectTrain
 } from './index';
-
 // subjectTrain()
 // behavioralSubjectTrain()
-replaySubjectTrain()
+// replaySubjectTrain()
 // asyncSubjectTrain()
+
+
+function trimmer(val:string):string {
+  return val.trim()
+}
+function splitter (val:string):string[] {
+  return val.split("")
+
+}
+function deleteAndStringer(arr:string[]) {
+  let result:string = "";
+  for (let i:number =0; i <arr.length - 2; i++) {
+     result = `${result}${arr[i]}`
+  }
+  return result
+}
+
+deleteAndStringer(splitter(trimmer("myNameis")))
